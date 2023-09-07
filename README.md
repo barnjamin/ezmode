bf95a03b0def9b13dd14aba5e7544431513184be
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-09-07 12:07:03 -0400

    bump versions

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-Ez Mode
+Ez Mode 
 -------
 
+Dummy simple demo for the [connect-sdk](https://github.com/wormhole-foundation/connect-sdk)
 
+# Setup
 
 ```sh
-npm install @wormhole-foundation/connect-sdk @wormhole-foundation/connect-sdk-evm @wormhole-foundation/connect-sdk-solana
+git clone git@github.com:barnjamin/ezmode.git
+cd ezmode
+npm i
+```
+
+This installs `@wormhole-foundation/connect-sdk` and 2 platform packages:
+
+- `@wormhole-foundation/connect-sdk-evm` 
+- `@wormhole-foundation/connect-sdk-solana`
+
+Add keys in a `.env` file like:
+
+```
+SOL_PRIVATE_KEY="BASE_58_PRIVATE_KEY"
+ETH_PRIVATE_KEY="BASE_16_PRIVATE_KEY"
+```
+
+# Run it
+
+The program provided executes a simple manual token transfer from Avalanche to Celo.
+
+```sh
+npm run doit
 ```
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-import { Wormhole } from "@wormhole-foundation/connect-sdk";
+import { ChainName, Wormhole } from "@wormhole-foundation/connect-sdk";
 import { EvmPlatform } from "@wormhole-foundation/connect-sdk-evm";
-import { fmtForDisplay } from "./helpers";
+import { fmtForDisplay, getStuff } from "./helpers";
 
 (async function () {
   const wh = new Wormhole("Testnet", [EvmPlatform]);
 
+  // Native is the gas token for any chain (e.g. Eth for Ethereum, Sol for Solana, etc..)
   const token = "native";
-  const address = "0x6603b4a7E29DfBDB6159c395a915e74757c1FB13";
 
-  const balance = await wh.getBalance("Ethereum", token, address);
-  const decimals = await wh.getDecimals("Ethereum", token);
+  // Get some signers
+  const { signer: fromSigner, address: fromAddress } = await getStuff(
+    wh.getChain("Avalanche")
+  );
+  const { signer: toSigner, address: toAddress } = await getStuff(
+    wh.getChain("Celo")
+  );
 
-  console.log(fmtForDisplay(balance!, decimals, 8));
+  // Grab some balances
+  const chains: ChainName[] = ["Ethereum", "Avalanche", "Celo"];
+  for (const chain of chains) {
+    const balance = await wh.getBalance(chain, token, fromSigner.address());
+    const decimals = await wh.getDecimals(chain, token);
+    console.log(`Balance on ${chain}: ${fmtForDisplay(balance!, decimals, 8)}`);
+  }
+
+  // Make (manual) a token transfer
+  const xfer = await wh.tokenTransfer(
+    token,
+    1_000_000_000_000n,
+    fromAddress,
+    toAddress,
+    false
+  );
+  console.log(xfer);
+
+  const srcTxIds = await xfer.initiateTransfer(fromSigner);
+  console.log("Initiated transfer with txids: ", srcTxIds);
+
+  const attestation = await xfer.fetchAttestation();
+  console.log("Got attestation: ", attestation);
+
+  const dstTxIds = await xfer.completeTransfer(toSigner);
+  console.log("Completed transfer with txids: ", dstTxIds);
 })();
