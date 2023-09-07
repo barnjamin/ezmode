5f745e7a134fc6417898c42a524ac39367ea5e34
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-09-07 09:53:37 -0400

    tweak versions

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import { Wormhole, Context, Network } from "@wormhole-foundation/connect-sdk";
-import { EvmContext } from "@wormhole-foundation/connect-sdk-evm";
-import { SolanaContext } from "@wormhole-foundation/connect-sdk-solana";
+import { Wormhole } from "@wormhole-foundation/connect-sdk";
+import { EvmPlatform } from "@wormhole-foundation/connect-sdk-evm";
 
 (async function () {
-  const NETWORK = Network.MAINNET;
-  const contexts = {
-    [Context.EVM]: EvmContext,
-    [Context.SOLANA]: SolanaContext,
-  };
+  const wh = new Wormhole("Testnet", [EvmPlatform]);
 
-  const wormholeSDK = new Wormhole(NETWORK, contexts);
-  const receipt = wormholeSDK.startTransfer(
-    "native",
-    100n, // amount
-    "solana", // sending chain
-    "...", // sender address
-    "ethereum", // destination chain
-    "..." // recipient address on destination chain
-  );
+  const token = "native";
+  const address = "0x6603b4a7E29DfBDB6159c395a915e74757c1FB13";
 
-  //
+  const balance = await wh.getBalance("Ethereum", token, address);
+  const decimals = await wh.getDecimals("Ethereum", token);
+
+  console.log(fmtForDisplay(balance!, decimals, 8));
 })();
+
+function fmtForDisplay(
+  value: bigint,
+  actual_decimals: bigint,
+  display_decimals: number
+): number {
+  const fixedPlace =
+    value / 10n ** (actual_decimals - BigInt(display_decimals));
+  return Number(fixedPlace) / 10 ** display_decimals;
+}
